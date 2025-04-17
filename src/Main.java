3664aa74ac4cda3d65815ff331d4d2d8a85c2401
владимир <sitnik792@gmail.ru>
2025-04-17 18:10:47 +0300

    hw5

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,31 @@ public class Main {
         int clientDeviceYear = 2015;
         if (clientOS == 0) {
             if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+                System.out.println("Установите облегчённую версию приложения для iOS по ссылке.");
             } else {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для iOS по ссылке.");
+                }
             }
         } else {
             if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+                System.out.println("Установите облегчённую версию приложения для Android по ссылке.");
             } else {
-                System.out.println("Установите версию приложения для Android по ссылке.");
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+                }
             }
         }
 
         System.out.println("задание 3");
         int year = 2021;
         if (year > 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year + " год является високосным.");
+            if (year % 4 == 0) {
+                if (year % 100 != 0 || year % 400 == 0) {
+                    System.out.println(year + " год является високосным.");
+                } else {
+                    System.out.println(year + " год не является високосным.");
+                }
             } else {
                 System.out.println(year + " год не является високосным.");
             }
